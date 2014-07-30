8e3b05fe1620cc4bed0ad29abb534fa39b78dcdd
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-30 16:25:45 -0400

    live bitcoin bid, ask, and last feed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('The Iron Yard Rocks');
+
+
+var Firebase;
+
+var bitcoinRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin");
+
+bitcoinRef.child("bid").on("value", showPrice);
+bitcoinRef.child("ask").on("value", showPrice);
+bitcoinRef.child("last").on("value", showPrice);
+
+function showPrice(snapshot) {
+  console.log(snapshot.name() + ": " + snapshot.val());
+}
