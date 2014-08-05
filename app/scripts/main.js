f7d48db4ec16ae056da71c7dbae8e4567c46c2d3
Jonathan Dickerson <jongdickerson@gmail.com>
2014-08-05 16:41:57 -0400

    created btc arrays

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,9 @@
 
 
-var Firebase;
-
-var Previous = {
-	bid: [],
-	ask: [],
-	last: []
-};
+btc_bid = [];
+btc_ask = [];
+btc_last = [];
+// btc_recent = [];
 
 var bitcoinRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin");
 
@@ -21,16 +18,16 @@ function showPrice(snapshot) {
 	var data = {
 			price: snapshot.name(),
 			value: snapshot.val(),
-			timestamp : (function() { return new Date() })()
+			timestamp : (function() { return new Date()})()
 	}
 
 	if(snapshot.name() == "bid"){
 
-		if(Previous.bid.length != 0){
-			var bid_length = Previous.bid.length - 1;
-			var bid_last = Previous.bid[bid_length].value;
+		if(btc_bid.length != 0){
+			var bid_length = btc_bid.length - 1;
+			var bid_last = btc_bid[bid_length].value;
 
-			$(".value_feed").addClass("value_feed_size");
+			// $(".value_feed").addClass("value_feed_size");
 
 			if(snapshot.val() > bid_last){
 				data.value_color = 'value_color_green';
@@ -39,10 +36,12 @@ function showPrice(snapshot) {
 			}
 		}
 
-
-
 		$(".bid_action").prepend(template(data));
-		Previous.bid.push(data);
+		btc_bid.push(data);
+
+		// if(btc_bid.length > 3){
+		// 	btc_bid.shift();
+		// }
 	}
 
   if(snapshot.name() == "ask"){
@@ -55,7 +54,7 @@ function showPrice(snapshot) {
 		//var last_push = Previous.last.push(data);
   }
 
-  console.log(Previous);
+  console.log(btc_bid);
   // console.log(Previous.bid[0].timestamp);
 
 
@@ -70,7 +69,7 @@ function showPrice(snapshot) {
               x: 0
           },
           xAxis: {
-              categories: [Previous.bid[0].timestamp, '2', '3', '4', '5', '6',
+              categories: [btc_bid[0].timestamp.getHours() + ':' + btc_bid[0].timestamp.getMinutes() + ':' + btc_bid[0].timestamp.getSeconds(), '2', '3', '4', '5', '6',
                   '7', '8', '9', '10', '11', '12']
   
           },
@@ -95,7 +94,7 @@ function showPrice(snapshot) {
           },
           series: [{
               name: 'Bid',
-              data: [400, 410, 450, 500, 580, 610, 575, 555]
+              data: [410, 450, 500, 580, 610, 575, 555]
           // }, {
           //     name: 'Last',
           //     data: [0.0, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
@@ -104,7 +103,9 @@ function showPrice(snapshot) {
           //     data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
           }]
       });
+		// btc_bid.value.push(data);
   });
+
 }
 
 
